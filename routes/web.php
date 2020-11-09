258d819ed035970c8842914d0fd915ae454e4d20
victor <victorcs2704@gmail.com>
2020-11-08 23:02:57 -0400

    183. Usando el Componente para Gestionar Tokens Personales

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //Auth::routes();
 
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function () {
